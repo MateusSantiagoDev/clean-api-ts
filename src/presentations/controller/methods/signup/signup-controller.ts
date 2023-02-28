1b8475a910e6c99f6e30f4ee1d73e471460367d7
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-28 18:06:55 -0300

    feat: garantir o retorno 500 se AddAccount falhar

--- src/presentations/controller/methods/signup/signup-controller.ts
+++ src/presentations/controller/methods/signup/signup-controller.ts
@@ -1,18 +1,22 @@
 import { AddAccount } from '../../../../domain/usecase/add-account'
+import { serverError } from '../../../helpers/http/http-helper'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 
 export class SignUpController implements Controller {
   constructor (private readonly addAccount: AddAccount) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {  
-    
-    const { name, email, password } = httpRequest.body
-    await this.addAccount.add({
-      name,
-      email,
-      password
-    })    
-    
-    return new Promise(resolve => resolve(null))
+    try {
+      const { name, email, password } = httpRequest.body
+      await this.addAccount.add({
+        name,
+        email,
+        password
+      })    
+      
+      return null
+    } catch (err) {
+      return serverError(err)
+    }
   }
 }
