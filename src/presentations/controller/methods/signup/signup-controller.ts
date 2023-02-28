13d1608c4b1bc3d86770ef50775b133581e3d7c3
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-28 18:46:36 -0300

    feat: garantir a integração do Authentication com valores corretos

--- src/presentations/controller/methods/signup/signup-controller.ts
+++ src/presentations/controller/methods/signup/signup-controller.ts
@@ -1,14 +1,17 @@
 import { AddAccount } from '../../../../domain/usecase/add-account'
+import { Authentication } from '../../../../domain/usecase/authentication'
 import { EmailinUseError } from '../../../errors'
 import { forbidden, serverError } from '../../../helpers/http/http-helper'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 
 export class SignUpController implements Controller {
-  constructor (private readonly addAccount: AddAccount) {}
+  constructor (private readonly addAccount: AddAccount, private readonly authentication: Authentication) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {  
     try {
+
       const { name, email, password } = httpRequest.body
+      
       const account = await this.addAccount.add({
         name,
         email,
@@ -16,7 +19,9 @@ export class SignUpController implements Controller {
       })
       if (!account) {
         return forbidden(new EmailinUseError())
-      }   
+      } 
+      
+      await this.authentication.auth({ email, password })
       
       return null
     } catch (err) {
