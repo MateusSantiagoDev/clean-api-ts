3e3ab8387a282cac308f234615d5f325c272c2d1
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-28 16:06:57 -0300

    feat: garantor o retorno 200 se credenciais válidas forem fornecidas

--- src/presentations/controller/methods/login/login-controller.ts
+++ src/presentations/controller/methods/login/login-controller.ts
@@ -1,18 +1,18 @@
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Authentication } from '../../../../domain/usecase/authentication'
-import { serverError, unauthorized } from '../../../helpers/http/http-helper'
+import { ok, serverError, unauthorized } from '../../../helpers/http/http-helper'
 
 export class LoginController implements Controller {
   constructor (private readonly autentication: Authentication) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { email, password } = httpRequest.body
-      const accessToken = this.autentication.auth({ email, password })
+      const accessToken = await this.autentication.auth({ email, password })
       if (!accessToken) {
         return unauthorized()
       }
-      return new Promise(resolve => resolve(null))
+      return ok({ accessToken })
     } catch (err) {
       return serverError(err)
     }
