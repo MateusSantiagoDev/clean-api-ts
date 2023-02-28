9cb1e48c91b38d48502fcb8a81ea38ad8f4868fb
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-28 16:29:08 -0300

    test: garantir Validação com valores corretos

--- src/presentations/controller/methods/login/login-controller.spec.ts
+++ src/presentations/controller/methods/login/login-controller.spec.ts
@@ -2,6 +2,7 @@ import { LoginController } from './login-controller'
 import { Authentication, AuthenticationDto } from '../../../../domain/usecase/authentication'
 import { HttpRequest } from '../../../protocols/http'
 import { serverError, unauthorized, ok } from '../../../helpers/http/http-helper'
+import { Validation } from '../../../protocols/validation'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -9,6 +10,15 @@ const makeFakeRequest = (): HttpRequest => ({
     password: 'any_password'
   }
 })
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
 
 const makeAuthentication = (): Authentication => {
   class AuthenticationStub implements Authentication {
@@ -22,14 +32,17 @@ const makeAuthentication = (): Authentication => {
 interface sutTypes {
   authenticationStub: Authentication
   sut: LoginController
+  validationStub: Validation
 } 
 
 const makesut = (): sutTypes => {
+  const validationStub = makeValidation()
   const authenticationStub = makeAuthentication()
-  const sut = new LoginController(authenticationStub)
+  const sut = new LoginController(authenticationStub, validationStub)
   return {
     sut,
-    authenticationStub
+    authenticationStub,
+    validationStub
   }
 }
 
@@ -52,16 +65,25 @@ describe('Login Controller', () => {
     expect(httpResponse).toEqual(unauthorized())
   })
 
-  /*   test('Deve retornar 500 se a autenticação falhar', async () => {
+  test('Deve retornar 500 se a autenticação falhar', async () => {
     const { sut, authenticationStub } = makesut()
     jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
-  })  */
+  })  
 
   test('Deve retornar 200 se credenciais válidas forem fornecidas', async () => {
     const { sut } = makesut()
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }))
+  })
+
+  // integração
+  test('Deve chamar Validação com valores corretos', async () => {
+    const { sut, validationStub } = makesut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 })
