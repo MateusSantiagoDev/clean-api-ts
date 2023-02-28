dc66be77e2029ea2610a60c3a29f374365f66cb1
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-28 16:28:55 -0300

    feat: garantir Validação com valores corretos

--- src/presentations/controller/methods/login/login-controller.ts
+++ src/presentations/controller/methods/login/login-controller.ts
@@ -1,12 +1,17 @@
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Authentication } from '../../../../domain/usecase/authentication'
-import { ok, serverError, unauthorized } from '../../../helpers/http/http-helper'
+import { badRequest, ok, serverError, unauthorized } from '../../../helpers/http/http-helper'
+import { Validation } from '../../../protocols/validation'
 
 export class LoginController implements Controller {
-  constructor (private readonly autentication: Authentication) {}
+  constructor (private readonly autentication: Authentication, private readonly validation: Validation) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = await this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
       const { email, password } = httpRequest.body
       const accessToken = await this.autentication.auth({ email, password })
       if (!accessToken) {
