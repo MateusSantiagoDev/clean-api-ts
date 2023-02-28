7c5e8b1a2174a353f027fa1b2f6f31cc8c5c3a7c
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-28 19:43:47 -0300

    feat: garantir a integração do Validation com valores corretos

--- src/presentations/controller/methods/signup/signup-controller.ts
+++ src/presentations/controller/methods/signup/signup-controller.ts
@@ -1,17 +1,20 @@
 import { AddAccount } from '../../../../domain/usecase/add-account'
 import { Authentication } from '../../../../domain/usecase/authentication'
 import { EmailinUseError } from '../../../errors'
-import { forbidden, serverError } from '../../../helpers/http/http-helper'
+import { badRequest, forbidden, serverError } from '../../../helpers/http/http-helper'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
+import { Validation } from '../../../protocols/validation'
 
 export class SignUpController implements Controller {
-  constructor (private readonly addAccount: AddAccount, private readonly authentication: Authentication) {}
+  constructor (private readonly addAccount: AddAccount, private readonly authentication: Authentication, private readonly validation: Validation) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {  
     try {
 
+      this.validation.validate(httpRequest.body)
+
       const { name, email, password } = httpRequest.body
-      
+
       const account = await this.addAccount.add({
         name,
         email,
