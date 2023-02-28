730cbb57f345833b0b8cddf5b82e0136cd7145c8
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-28 16:07:11 -0300

    test: garantor o retorno 200 se credenciais válidas forem fornecidas

--- src/presentations/controller/methods/login/login-controller.spec.ts
+++ src/presentations/controller/methods/login/login-controller.spec.ts
@@ -1,7 +1,7 @@
 import { LoginController } from './login-controller'
 import { Authentication, AuthenticationDto } from '../../../../domain/usecase/authentication'
 import { HttpRequest } from '../../../protocols/http'
-import { serverError, unauthorized } from '../../../helpers/http/http-helper'
+import { serverError, unauthorized, ok } from '../../../helpers/http/http-helper'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -52,10 +52,16 @@ describe('Login Controller', () => {
     expect(httpResponse).toEqual(unauthorized())
   })
 
-  test('Deve retornar 500 se a autenticação falhar', async () => {
+  /*   test('Deve retornar 500 se a autenticação falhar', async () => {
     const { sut, authenticationStub } = makesut()
     jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
-  }) 
+  })  */
+
+  test('Deve retornar 200 se credenciais válidas forem fornecidas', async () => {
+    const { sut } = makesut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }))
+  })
 })
