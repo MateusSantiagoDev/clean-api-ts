80b0313b9e0afb72d76f7a64a38c2f25d3ceae39
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-28 18:46:48 -0300

    test: garantir a integração do Authentication com valores corretos

--- src/presentations/controller/methods/signup/signup-controller.spec.ts
+++ src/presentations/controller/methods/signup/signup-controller.spec.ts
@@ -2,8 +2,9 @@ import { SignUpController } from './signup-controller'
 import { AddAccount, AccountDto} from '../../../../domain/usecase/add-account'
 import { AccountModel } from '../../../../domain/model/account'
 import { HttpRequest } from '../login/login-controller-protocols'
-import { forbidden, serverError } from '../../../helpers/http/http-helper'
+import { forbidden, ok, serverError } from '../../../helpers/http/http-helper'
 import { EmailinUseError, ServerError } from '../../../errors'
+import { Authentication, AuthenticationDto} from '../../../../domain/usecase/authentication'
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'any_id',
@@ -21,6 +22,15 @@ const makeFakeRequest = (): HttpRequest => ({
   }
 })
 
+const makeAuthentication = (): Authentication => {
+  class AuthenticationStub implements Authentication {
+    async auth (authentication: AuthenticationDto): Promise<string> {
+      return new Promise(resolve => resolve('any_token'))
+    }
+  }
+  return new AuthenticationStub()
+}
+
 const makeAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
     async add(account: AccountDto): Promise<AccountModel> {
@@ -33,20 +43,23 @@ const makeAccount = (): AddAccount => {
 interface sutTypes {
   sut: SignUpController
   addAccountStub: AddAccount
+  authenticationStub: Authentication
 }
 
 const makeSut = (): sutTypes => {
+  const authenticationStub = makeAuthentication()
   const addAccountStub = makeAccount()
-  const sut = new SignUpController(addAccountStub)
+  const sut = new SignUpController(addAccountStub, authenticationStub)
   return {
     sut,
-    addAccountStub
+    addAccountStub,
+    authenticationStub
   }
 }
 
 describe('Signup Controller', () => {
   // teste de integração
-  test('Deve chamar AddAccount com valores corretos', async () => {
+  test('Deve chamar o AddAccount com valores corretos', async () => {
     const { sut, addAccountStub } = makeSut()
     const accounSpy = jest.spyOn(addAccountStub, 'add')
     await sut.handle(makeFakeRequest())
@@ -66,9 +79,17 @@ describe('Signup Controller', () => {
   })
 
   test('Deve retornar 403 se AddAccount retornar nulo', async () => {
-    const {sut, addAccountStub } = makeSut()
+    const { sut, addAccountStub } = makeSut()
     jest.spyOn(addAccountStub, 'add').mockReturnValueOnce(new Promise(resolve => resolve(null)))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(forbidden(new EmailinUseError()))
+  })
+
+  // integração
+  test('Deve chamar o Authentication com valores corretos', async () => {
+    const { sut, authenticationStub } = makeSut()
+    const authSpy = jest.spyOn(authenticationStub, 'auth')
+    await sut.handle(makeFakeRequest())
+    expect(authSpy).toHaveBeenCalledWith({ email: 'any_email@mail.com', password: 'any_password' })
   })
 })
