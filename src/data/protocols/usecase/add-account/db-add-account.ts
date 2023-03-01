5b606e04b453374a4ddc4330620d053bbd00348b
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-01 01:02:30 -0300

    feat: garantir o retorno do LoadAccountByEmailRepository com o email correto

--- src/data/protocols/usecase/add-account/db-add-account.ts
+++ src/data/protocols/usecase/add-account/db-add-account.ts
@@ -2,11 +2,17 @@ import { AccountModel } from '../../../../domain/model/account'
 import { AccountDto, AddAccount } from '../../../../domain/usecase/add-account'
 import { Hasher } from '../../criptography/hasher'
 import { AddAccountRepository } from '../../db/account/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../db/account/load-account-by-email-repository'
 
 export class DbAccount implements AddAccount {
-  constructor (private readonly hasher: Hasher, private readonly addAccountRepository: AddAccountRepository) {}
+  constructor (private readonly hasher: Hasher, private readonly addAccountRepository: AddAccountRepository, private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
   async add (accountData: AccountDto): Promise<AccountModel> {
-    const hashedPassword = await this.hasher.hash(accountData.password)
-    return await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+    // se o email não existir ele cria uma conta, se ja existir ele retorna null
+    const account = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+    if (!account) {
+      const hashedPassword = await this.hasher.hash(accountData.password)
+      return await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+    }
+    return null
   }
 }
