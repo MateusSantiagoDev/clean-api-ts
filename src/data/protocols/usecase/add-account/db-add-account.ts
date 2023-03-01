a700c52463c09b24f5b28ece59fd919e48d2690b
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-01 00:36:08 -0300

    feat: garantor o retorno de AddAccountRepository com valores corretos

--- src/data/protocols/usecase/add-account/db-add-account.ts
+++ src/data/protocols/usecase/add-account/db-add-account.ts
@@ -1,11 +1,12 @@
 import { AccountModel } from '../../../../domain/model/account'
 import { AccountDto, AddAccount } from '../../../../domain/usecase/add-account'
 import { Hasher } from '../../criptography/hasher'
+import { AddAccountRepository } from '../../db/account/add-account-repository'
 
 export class DbAccount implements AddAccount {
-  constructor (private readonly hasher: Hasher) {}
-  async add (account: AccountDto): Promise<AccountModel> {
-    await this.hasher.hash(account.password)
-    return new Promise(resolve => resolve(null))  
+  constructor (private readonly hasher: Hasher, private readonly addAccountRepository: AddAccountRepository) {}
+  async add (accountData: AccountDto): Promise<AccountModel> {
+    const hashedPassword = await this.hasher.hash(accountData.password)
+    return await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
   }
 }
