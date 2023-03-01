140014654444173a8578dc3fa260126355205fd2
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-01 17:02:55 -0300

    test: garantir o retorno do EmailValidator com o email correto

---
+++ src/validation/validators/email-validation.spec.ts
@@ -0,0 +1,35 @@
+import { EmailValidation } from './email-validation'
+import { EmailValidator } from '../protocols/email-validator'
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+interface sutTypes {
+  sut: EmailValidation
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): sutTypes => {
+  const emailValidatorStub = makeEmailValidator()
+  const sut = new EmailValidation('email', emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
+
+describe('Email Validation', () => {
+  // integração
+  test('Deve chamar o EmailValidator com o email correto', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    sut.validate({ email: 'any_email@mail.com' })
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+})
