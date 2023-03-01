38dd273a11c8c4cd0f01e74a29ddf3defe3d1e87
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-01 16:09:58 -0300

    feat: garantir o retorno de um InvalidParamError se o validation falhar

---
+++ src/validation/validators/compare-field-validation.ts
@@ -0,0 +1,14 @@
+import { Validation } from '../../presentations/protocols'
+import { InvalidParamError } from '../../presentations/errors'
+
+export class CompareFieldsValidation implements Validation {
+  constructor (
+    private readonly fieldName: string, 
+    private readonly fieldTocampareName: string) {}
+
+  validate (input: any): Error {
+    if (input[this.fieldName] !== input[this.fieldTocampareName]) {
+      return new InvalidParamError(this.fieldTocampareName)
+    }
+  }
+}
