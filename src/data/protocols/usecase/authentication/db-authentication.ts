51a08e2265e50a4a38b5d2788b15d98c9027041c
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-01 13:09:38 -0300

    feat: gharantir o retorno do UpdateAccessTokenRepository com valores corretos

--- src/data/protocols/usecase/authentication/db-authentication.ts
+++ src/data/protocols/usecase/authentication/db-authentication.ts
@@ -2,20 +2,27 @@ import { Authentication, AuthenticationDto } from '../../../../domain/usecase/au
 import { LoadAccountByEmailRepository } from '../../db/account/load-account-by-email-repository'
 import { HashCompare } from '../../criptography/hash-compare'
 import { Encrypter } from '../../criptography/encrypter'
+import { UpdateAccessTokenRepository } from '../../db/account/update-access-token-repository'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository, 
     private readonly hashCompare: HashCompare,
-    private readonly encrypter: Encrypter) {}
+    private readonly encrypter: Encrypter,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository) {}
 
   async auth (authentication: AuthenticationDto): Promise<string> {
     // se o email informado pelo usuário na existir ele cria se existir retorna null
-    const accessToken = await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
-    if (accessToken) {
+    const account = await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
+    if (account) {
       // comparando o password informado pelo usuário com a hashed_password salva no banco 
-      await this.hashCompare.compare(authentication.password, accessToken.password)
-      await this.encrypter.encrypt(accessToken.id)
+      const isValid = await this.hashCompare.compare(authentication.password, account.password)
+      if (isValid) {
+        const accessToken = await this.encrypter.encrypt(account.id)
+        // armazenando o id e o token no bandco de dados
+        await this.updateAccessTokenRepository.updateAccessToken(account.id, accessToken)
+        return accessToken
+      }
     } 
     return null 
   }
