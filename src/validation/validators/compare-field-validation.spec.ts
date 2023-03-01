39e1e6351a47e9a1a836737831e136c769cc65c3
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-01 16:17:00 -0300

    test: garantir que não seja retornado nada se a validação for bem-sucedida

--- src/validation/validators/compare-field-validation.spec.ts
+++ src/validation/validators/compare-field-validation.spec.ts
@@ -14,6 +14,11 @@ describe('CompareField Validation', () => {
     expect(error).toEqual(new InvalidParamError('fieldToCompare'))
   })
 
-  
-
+  // teste vai garantir que em caso de não houver erro
+  // não sera retornado nada
+  test('Não deve retornar se a validação for bem-sucedida', () => {
+    const sut = makeSut()
+    const error = sut.validate({field: 'any_value', fieldToCompare: 'any_value'})
+    expect(error).toBeFalsy()
+  })
 })
