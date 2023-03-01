7fd20c5f9365b472403ed085909d75f7e63ba5d0
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-01 17:14:26 -0300

    test: garantir o retorno de um erro se o EmailValidator retornar false

--- src/validation/validators/email-validation.spec.ts
+++ src/validation/validators/email-validation.spec.ts
@@ -1,5 +1,6 @@
 import { EmailValidation } from './email-validation'
 import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../../presentations/errors'
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
@@ -31,5 +32,12 @@ describe('Email Validation', () => {
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
     sut.validate({ email: 'any_email@mail.com' })
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
+  test('Deve retornar um erro se EmailValidator retornar false', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const isEmail = sut.validate({ email: 'any_email@mail.com' })
+    expect(isEmail).toEqual(new InvalidParamError('email'))
   })
 })
