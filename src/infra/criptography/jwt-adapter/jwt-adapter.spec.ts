b4e5af05009fca0ce517162b3f653f277da5f837
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-02 16:01:20 -0300

    test: garantir o retorno da falha se o Sign falhar

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,16 +1,30 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return new Promise(resolve => resolve('any_token'))
+  }
+}))
+
 const makeSut = (): JwtAdapter => {
   return new JwtAdapter('secret')
 }
 
 describe('JWT Adapter', () => {
   // integração
-  test('Deve chamar o sign com os valores corretos', async () => {
+  test('Deve chamar o Sign com os valores corretos', async () => {
     const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  // excessão
+  test('Deve falhar se o Sign falhar', async () => {
+    const sut = makeSut()
+    jest.spyOn<any, string>(jwt, 'sign').mockReturnValueOnce(new Promise((resolve,reject) => reject(new Error())))
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
   })
 })
--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -3,8 +3,8 @@ import { Encrypter } from '../../../data/protocols/criptography/encrypter'
 
 export class JwtAdapter implements Encrypter {
   constructor (private readonly secret: string) {}
+  
   async encrypt(value: string): Promise<string> {
-    Jwt.sign({ id: value }, this.secret)
-    return new Promise(resolve => resolve(null)) 
+    return Jwt.sign({ id: value }, this.secret)    
   }
 }
