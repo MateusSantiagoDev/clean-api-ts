069fcf0a8918993e54d9fb1b4d52d9f0e1399335
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-04 13:01:52 -0300

    test: garantir o retorno do tipo de conteúdo xml quando forçado

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -10,4 +10,14 @@ describe('Content Type Middleware', () => {
       .get('/test_content_type')
       .expect('Content-Type', /json/)
   })
+
+  test('Deve retornar o tipo de conteúdo xml quando forçado', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('Content-Type', /xml/)
+  })
 })
