3ecaf12e7601f988c009dc4373973c7f53e01a63
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-04 12:45:47 -0300

    feat: garantor que o CORS seja habilitado

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { BodyParser } from '../middlewares/body-parser'
+import { Cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
   app.use(BodyParser)
+  app.use(Cors)
 }
