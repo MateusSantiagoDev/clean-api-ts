85ee2070f970ec9d366a6a42f4f271208d20c37e
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-04 01:28:06 -0300

    feat: garantir o retorno do usuário com o loadByEmail criado com sucesso

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,12 +1,20 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository'
 import { AccountModel } from '../../../../domain/model/account'
 import { AccountDto } from '../../../../domain/usecase/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AccountDto): Promise<AccountModel> {
-    const getCollection = await MongoHelper.getCollection('account')
+    const getCollection = await MongoHelper.getCollection('accounts')
     const result = await getCollection.insertOne(accountData)
-    return MongoHelper.mapper(result.insertedId)
+    return await MongoHelper.mapper(result.insertedId)
+   
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const getCollection = await MongoHelper.getCollection('accounts')
+    const account = await getCollection.findOne({ email })
+    return account && MongoHelper.mapper(account)
   }
 }
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -10,7 +10,7 @@ export const MongoHelper = {
   },
 
   async disconnect () {
-    await this.Client.disconnect()
+    await this.Client.close()
     this.Client = null
   },
 
