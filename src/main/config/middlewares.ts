5f8e8baac91e7154ea0b78055bc10f5418e4ed53
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-04 12:57:09 -0300

    feat: garantir que o tipo de conteúdo padrão como json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { BodyParser } from '../middlewares/body-parser'
 import { Cors } from '../middlewares/cors'
+import { ContentType } from '../middlewares/content-type'
 
 export default (app: Express): void => {
   app.use(BodyParser)
   app.use(Cors)
+  app.use(ContentType)
 }
---
+++ src/main/middlewares/content-type.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from 'express'
+
+export const ContentType = (req: Request, res: Response, next: NextFunction): void => {
+  res.type('json')
+  next()
+}
