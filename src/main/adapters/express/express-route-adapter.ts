2d3e31a03cddd5d55e9d241f9d85fad2275a8598
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-04 14:25:12 -0300

    feat: garantir a integração do LogControllerDecorator e o controller com os valores corretor

---
+++ src/main/adapters/express/express-route-adapter.ts
@@ -0,0 +1,16 @@
+import { Controller, HttpRequest } from '../../../presentations/protocols'
+import { Request, Response } from 'express'
+
+export const AdapterRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    if (httpResponse.statusCode >= 200 || httpResponse.statusCode <= 299) {
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    } else {
+      res.status(httpResponse.statusCode).json({ error: httpResponse.body.message })
+    }
+  }
+}
