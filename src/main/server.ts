a80ce9b42bd13bc0ece06b46c46ee53e86ec117c
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-04 12:28:30 -0300

    feat: garantir analise do corpo da requisição como json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,4 @@
 import app from './config/app'
+import env from './config/env'
 
-app.listen(() => console.log('Servidor rodando em http://localhost:5050'))
+app.listen(env.port, () => console.log(`Servidor rodando em http://localhost:${env.port}`))
