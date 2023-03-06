49d8cec18ac38f57b710d91c3a96109975ff5605
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-06 00:21:34 -0300

    refactor: atualização do projeto para fazer deploy

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/api-node-clean',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/api-node-clean',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '3er7@!gy587',
 }
