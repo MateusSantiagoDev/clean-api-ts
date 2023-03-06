b07c8d99aa5c39f9f5b58e943a53415a43a4fe09
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-05 22:19:02 -0300

    feat: garantir que o validaçãoComposite seja chamado com todas as validações

---
+++ src/main/factories/controllers/login/login-validation-factory.ts
@@ -0,0 +1,14 @@
+import { EmailValidation, RequiredFieldValidation, ValidationComposite} from '../../../../validation/validators'
+import { Validation } from '../../../../presentations/protocols/validation'
+import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
+
+export const MakeLoginValidation = (): ValidationComposite => {
+  
+  const validations: Validation[] = []
+
+  for (const field of ['email', 'password']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+  return new ValidationComposite(validations)
+}
