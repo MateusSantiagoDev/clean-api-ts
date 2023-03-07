79659f49d422452fb6e602fb9285c5e0157e71a0
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-06 23:24:54 -0300

    feat: garantir o retorno 500 se AddSurvey falhar

--- src/presentations/controller/methods/survey/add-survey-controller.ts
+++ src/presentations/controller/methods/survey/add-survey-controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../login/login-controller-protocols'
 import { Validation } from '../../../protocols/validation'
-import { badRequest } from '../../../helpers/http/http-helper'
+import { badRequest, serverError } from '../../../helpers/http/http-helper'
 import { AddSurvey } from '../../../../domain/usecase/add-survey'
 
 export class AddSurveyController implements Controller {
@@ -11,17 +11,22 @@ export class AddSurveyController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
-    }
+    try {
 
-    const { question, answers } = httpRequest.body
-    await this.addSurvey.add({
-      question,
-      answers
-    })
-    
-    return new Promise(resolve => resolve(null))
-  }  
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+
+      const { question, answers } = httpRequest.body
+      await this.addSurvey.add({
+        question,
+        answers
+      })
+  
+      return new Promise(resolve => resolve(null))
+    } catch (error) {
+      return serverError(error)
+    }  
+  }
 }
