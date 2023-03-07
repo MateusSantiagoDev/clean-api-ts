91f1b8fa4eec9a7228b30b0152bb60d9629917c8
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-06 22:52:56 -0300

    feat: garantir o retorno 400 se a validação falhar

--- src/presentations/controller/methods/survey/add-survey-controller.ts
+++ src/presentations/controller/methods/survey/add-survey-controller.ts
@@ -1,11 +1,15 @@
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../login/login-controller-protocols'
 import { Validation } from '../../../protocols/validation'
+import { badRequest } from '../../../helpers/http/http-helper'
 
 export class AddSurveyController implements Controller {
   constructor (private readonly validation: Validation) {}
-  handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
     return new Promise(resolve => resolve(null))
   }  
 }
