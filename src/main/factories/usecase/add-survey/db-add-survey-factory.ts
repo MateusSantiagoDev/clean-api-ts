6fdd52791043b86dd9a95cb5fb5600fef4c0b87f
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-07 10:53:57 -0300

    feat: implementado o caso de uso MakeDbAddSurvey

---
+++ src/main/factories/usecase/add-survey/db-add-survey-factory.ts
@@ -0,0 +1,8 @@
+import { AddSurvey } from '../../../../domain/usecase/add-survey'
+import { SurveyMongoRepository } from '../../../../infra/db/mongodb/survey/survey-mongo-repository'
+import { DbAddSurvey } from '../../../../data/usecase/add-survey/db-add-survey'
+
+export const MakeDbAddSurvey = (): AddSurvey => {
+  const surveyMongoRepository = new SurveyMongoRepository()
+  return new DbAddSurvey(surveyMongoRepository)
+}
