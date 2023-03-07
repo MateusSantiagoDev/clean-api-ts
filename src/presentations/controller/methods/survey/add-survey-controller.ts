0271df2c575eb4038293a3f5a3d2aa43e410ee99
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-06 23:16:13 -0300

    feat: garantir a integração do AddSurvey com os valores corretos

--- src/presentations/controller/methods/survey/add-survey-controller.ts
+++ src/presentations/controller/methods/survey/add-survey-controller.ts
@@ -2,14 +2,26 @@ import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../login/login-controller-protocols'
 import { Validation } from '../../../protocols/validation'
 import { badRequest } from '../../../helpers/http/http-helper'
+import { AddSurvey } from '../../../../domain/usecase/add-survey'
 
 export class AddSurveyController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey,
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body)
     if (error) {
       return badRequest(error)
     }
+
+    const { question, answers } = httpRequest.body
+    await this.addSurvey.add({
+      question,
+      answers
+    })
+    
     return new Promise(resolve => resolve(null))
   }  
 }
