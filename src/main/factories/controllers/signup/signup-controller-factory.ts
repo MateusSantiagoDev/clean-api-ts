15fa2a35e97e871f7bacd3d755fac78779929c83
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-06 21:51:14 -0300

    adicionado arquivo add-survey-md

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from '../../../../presentations/controller/methods/signup/signup-controller'
 import { Controller } from '../../../../presentations/protocols'
-import { makeDbAuthentication } from '../../usecase/authentication/db-authentication-factory'
+/* import { makeDbAuthentication } from '../../usecase/authentication/ db-authentication-factory'*/
 import { makeDbAddAccount } from '../../usecase/add-account/db-add-account-factory'
 import { makeLogControllerDecorator } from '../../decorators/login-controller-decorator-factory'
 import { MakeSignupValidation } from './signup-validation-factory'
