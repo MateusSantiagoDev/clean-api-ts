1d18d333d864d308f3ccc63138fae48b03ab24e1
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-08 16:27:41 -0300

    feat: garantir que AuthMiddleware chame LoadAccountBytoken com a função correta

--- src/presentations/middlewares/auth-middleware.ts
+++ src/presentations/middlewares/auth-middleware.ts
@@ -7,13 +7,14 @@ import { LoadAccountByToken } from '../../domain/usecase/load-account-by-token'
 export class AuthMiddleware implements Middleware {
   constructor (
     private readonly loadAccountByToken: LoadAccountByToken,
+    private readonly role?: string,
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {    
       const accessToken = httpRequest.headers?.['x-access-token']
       if (accessToken) {
-        const account = await this.loadAccountByToken.load(accessToken)
+        const account = await this.loadAccountByToken.load(accessToken, this.role)
         if (account) {
           return ok({ accountId: account.id })
         }
