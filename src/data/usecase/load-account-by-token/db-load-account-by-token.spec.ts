c97d8a1121f82efb6b636aea611595c1e46f57cb
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-08 20:01:35 -0300

    test: garantir que DbLoadAccountByToken falhe se qualquer dependência falhar

--- src/data/usecase/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecase/load-account-by-token/db-load-account-by-token.spec.ts
@@ -87,4 +87,20 @@ describe('DbLoadAccountByToken Usecase', () => {
     const account = await sut.load('any_token', 'any_role')
     expect(account).toEqual(makeFakeAccount())
   })
+
+  // excessão
+  test('deve falhar se o Decrypter falhar', async () => {
+    const { sut, decrypterStub } = makeSut()
+    jest.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.load('any_token', 'any_role')
+    await expect(promise).rejects.toThrow()
+  })
+
+  // excessão
+  test('deve falhar se o LoadAccountByTokenRepository falhar', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.load('any_token', 'any_role')
+    await expect(promise).rejects.toThrow()
+  })
 })
