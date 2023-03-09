1d276f24d67696cef4ac808c1c627e8f60b3289e
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-08 21:04:30 -0300

    feat: garantir que o varify será chamado do jwtAdapter com os valores corretos

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,10 +1,16 @@
 import Jwt from 'jsonwebtoken'
+import { Decrypter } from '../../../data/protocols/criptography/decrypter'
 import { Encrypter } from '../../../data/protocols/criptography/encrypter'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
   
   async encrypt(value: string): Promise<string> {
     return Jwt.sign({ id: value }, this.secret)    
+  }
+
+  async decrypt (value: string): Promise<string> {
+    await Jwt.verify(value, this.secret)
+    return null
   }
 }
