b2f17c58d1eb66eaeecf1d685ec5c5d70017cf50
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-10 19:22:13 -0300

    feat: garantir que LoadSurveysController retorne 500 se loadSurveysUsecase falhar

--- src/presentations/controller/methods/survey/load-surveys/load-surveys-controller.ts
+++ src/presentations/controller/methods/survey/load-surveys/load-surveys-controller.ts
@@ -1,11 +1,15 @@
-import { ok } from './../../../../helpers/http/http-helper'
+import { ok, serverError } from './../../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadSurvey } from './load-surveys-controller-protocols'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurvey: LoadSurvey) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveys = await this.loadSurvey.load()
-    return ok(surveys) 
+    try {
+      const surveys = await this.loadSurvey.load()
+      return ok(surveys)
+    } catch(error) {
+      return serverError(error)
+    }
   }
 }
