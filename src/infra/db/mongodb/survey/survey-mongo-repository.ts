20355c4af48405bd5fe65773eb0b5c3cc8d412fa
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-03-13 16:23:22 -0300

    feat:

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,22 @@
 import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-surveys-repository'
+import { SurveyModel } from '../../../../domain/model/survey'
 import { AddSurveyDto } from '../../../../domain/usecase/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (surveyData: AddSurveyDto): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData) 
+  }
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys') 
+    const surveys = await surveyCollection.find().toArray() // trazer a lista de surveys
+    return surveys.map(survey => ({
+      id: survey._id.toString(),
+      question: survey.question,
+      answers: survey.answers,
+      date: survey.date,
+    }))
   }
 }
